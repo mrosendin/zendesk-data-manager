e18f5c25366d2d324e8e630754deb72399cbb08d
Matt Rosendin <matt.rosendin@gmail.com>
2017-06-12 17:59:02 -0700

    poll progress from server

--- src/components/shared/DownloadModal.vue
+++ src/components/shared/DownloadModal.vue
@@ -4,7 +4,7 @@
       <div class="modal-background"></div>
       <div class="modal-card">
         <header class="modal-card-head">
-          <p class="modal-card-title">Download Results</p>
+          <p class="modal-card-title">Download</p>
           <button class="delete" @click="onClose"></button>
         </header>
         <section class="modal-card-body">
@@ -46,15 +46,16 @@
               </div>
             </div>
           </div>
-          <div class="columns">
-            <div class="column">
-              <progress class="progress is-success" value="60" max="100">60%</progress>
+          <div class="columns" v-if="inProgress">
+            <div class="column has-text-centered">
+              <p>Please wait for the file processing to complete.</p>
+              <progress class="progress is-success" :value="progress" max="100">{{ progress }}%</progress>
             </div>
           </div>
         </section>
         <footer class="modal-card-foot pull-right">
-          <a class="button is-success" @click="start">Start</a>
-          <a class="button" @click="onClose">Cancel</a>
+          <a class="button is-success" @click="start" :disabled="inProgress">Start</a>
+          <a class="button" @click="cancel">Cancel</a>
         </footer>
       </div>
     </div>
@@ -62,6 +63,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import config from '../../common/config.js'
 
 export default {
@@ -82,19 +84,54 @@ export default {
     return {
       filename: '',
       fileType: 'csv',
-      warningMessage: ''
+      warningMessage: '',
+      inProgress: false,
+      progress: 0
     }
   },
   methods: {
     start () {
-      console.log('Start clicked.')
       if (!this.filename) {
         this.warningMessage = 'You must set a file name to begin the download.'
         return
       }
+
+      this.inProgress = true
       // get email/token, encrypt, create request and send as header
+
+
       // send the search url with parameters as body
+      axios({
+        method: 'post',
+        url: 'http://localhost:3000/api/data-manager/export',
+        data: {
+          filename: this.filename
+        }
+      }).then(response => {
+        let id = response.data.id
+        let poll = setInterval(() => {
+          console.log(`Retrieving status for job id ${id}`)
+          axios({
+            method: 'get',
+            url: `http://localhost:3000/api/data-manager/export/${id}`
+          }).then(response => {
+            this.progress = response.data.progress
+            if (this.progress === 100) clearInterval(poll)
+          }).catch(error => {
+            console.log(error)
+          })
+        }, 500)
+      }).catch(error => {
+        console.log(error)
+      })
+
       // poll the status endpoint and update progress bar
+
+
+    },
+    cancel () {
+      this.inProgress = false
+      this.onClose()
     }
   },
   computed: {
