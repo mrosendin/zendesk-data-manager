9065b54360a391647d4bb465510b15b819e0541a
Matt Rosendin <matt.rosendin@gmail.com>
2017-06-14 14:08:07 -0700

    update perPage to 100 for all metadata

--- src/components/metadata/Views.vue
+++ src/components/metadata/Views.vue
@@ -67,7 +67,7 @@ export default {
       columns: columns.viewColumns,
       results: [],
       resultCount: 0,
-      perPage: 30,
+      perPage: 100,
       messages: {
         success: '',
         error: ''
@@ -111,7 +111,7 @@ export default {
       format(data.views, 'views', this.columns).then(results => {
         this.results = results
         this.resultCount = data.count
-        this.perPage = 30
+        this.perPage = 100
       })
     }).catch(error => {
       console.log(error)
